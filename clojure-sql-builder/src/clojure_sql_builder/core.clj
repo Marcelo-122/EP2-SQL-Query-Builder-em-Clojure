c5ff5821a7ac3eb9f2d5f09e9ed0f3a9a48ee620
Marcelo Fernandes <marcelo.fojunior12@gmail.com>
2025-10-19 11:53:32 -0300

    Adição de ORDER BY e LIMIT

--- clojure-sql-builder/src/clojure_sql_builder/core.clj
+++ clojure-sql-builder/src/clojure_sql_builder/core.clj
@@ -1,5 +1,5 @@
 (ns clojure-sql-builder.core)
-
+(require '[clojure.string :as str])
 ;; Formata um valor para SQL (strings precisam de aspas simples)
 (defn formata-valor [valor]
   (if (string? valor)
@@ -21,8 +21,8 @@
       :maior_igual_que (str campo " >= " (formata-valor valor))
       :entre          (str campo " BETWEEN " (formata-valor valor))
       :like           (str campo " LIKE " (formata-valor valor)) 
-      :not_in (str campo " NOT IN (" (clojure.string/join ", " (map formata-valor valor)) ")")
-      :em        (str campo " IN (" (clojure.string/join ", " (map formata-valor valor)) ")")
+      :not_in (str campo " NOT IN (" (str/join ", " (map formata-valor valor)) ")")
+      :em        (str campo " IN (" (str/join ", " (map formata-valor valor)) ")")
       ;; outros comparadores aqui (:menor_que, :diferente_de, etc.)
       (throw (Exception. (str "Operador desconhecido: " op))))))
 
@@ -32,7 +32,7 @@
   (let [condicoes (second logica-fn) ; Pega o vetor de condições
         ;; Usa `map` para processar cada item da lista recursivamente
         partes (map processa-clausula condicoes)
-        clausula-final (clojure.string/join separador partes)]
+        clausula-final (str/join separador partes)]
     (str "(" clausula-final ")"))) ; Envolve em parênteses
   
 ;; Função "despachante": decide o que fazer com base no tipo de entrada
@@ -54,14 +54,16 @@
     (throw (Exception. (str "Cláusula inválida: " clausula)))))
   
 (defn to-sql [estado-query]
-  (let [campos (clojure.string/join ", " (:fields estado-query))
+  (let [campos (str/join ", " (:fields estado-query))
         tabela (:table estado-query)
-        where  (:where estado-query)]
-    (if where
-      (str "SELECT " campos " FROM " tabela " WHERE " where)
-      (str "SELECT " campos " FROM " tabela))))
+        where    (when (:where estado-query) (str "WHERE " (:where estado-query)))
+        order-by (:order-by estado-query)
+        limit (:limit estado-query)]
+    (clojure.string/join " " (filter some? ["SELECT" campos "FROM" tabela where order-by limit]))))
 
 ;; Funçoes builders
+
+
 (defn busca_tabela [nome-tabela]
   ;; O ponto de partida: cria o mapa de estado inicial
   {:table nome-tabela
@@ -76,6 +78,12 @@
   ;; Recebe o estado, processa a cláusula where e retorna um NOVO estado
   (assoc estado-query :where (processa-clausula clausula-where)))
 
+(defn order-by [estado-query ord-map]
+  (assoc estado-query :order-by (str "ORDER BY " (name (:campo ord-map)) " " (name (:direcao ord-map)))))
+
+(defn limit [estado-query limite]
+  (assoc estado-query :limit (str "LIMIT " limite)))
+
 ;; --- Teste no REPL ---
 ;; Usando a macro -> para encadear as chamadas de forma legível
 (-> (busca_tabela "usuario")
@@ -86,4 +94,6 @@
                          {:campo :status, :igual_a true}
                          (list 'ou_s [{:campo :camiseta, :igual_a "verde"}
                                       {:campo :camiseta, :igual_a "azul"}])]))
+    (order-by {:campo :idade :direcao :desc})
+    (limit 10)
     to-sql)
