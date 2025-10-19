0b66614312b5e86631e187bbc94eba8c515a01a6
Marcelo Fernandes <marcelo.fojunior12@gmail.com>
2025-10-19 10:53:52 -0300

    cláusulas lógicas
    - Função processa-clausula que processa clausulas logicas como AND e OR

--- clojure-sql-builder/src/clojure_sql_builder/core.clj
+++ clojure-sql-builder/src/clojure_sql_builder/core.clj
@@ -14,11 +14,41 @@
     (case op
       :igual_a   (str campo " = " (formata-valor valor))
       :maior_que (str campo " > " (formata-valor valor))
-      :em        (str campo " IN (" (clojure.string/join ", " valor) ")")
-      ;; Adicione outros comparadores aqui (:menor_que, :diferente_de, etc.)
+      :em        (str campo " IN (" (clojure.string/join ", " (map formata-valor valor)) ")")
+      ;; outros comparadores aqui (:menor_que, :diferente_de, etc.)
       (throw (Exception. (str "Operador desconhecido: " op))))))
 
-(defn -main [& _args]
-  (println (processa-condicao {:campo 'nome, :igual_a "José"}))
-  (println (processa-condicao {:campo 'idade, :maior_que 20}))
-  (println (processa-condicao {:campo 'id, :em [10, 20, 30]})))
+(declare processa-clausula)
+
+(defn processa-e-ou [logica-fn separador]
+  (let [condicoes (second logica-fn) ; Pega o vetor de condições
+        ;; Usa `map` para processar cada item da lista recursivamente
+        partes (map processa-clausula condicoes)
+        clausula-final (clojure.string/join separador partes)]
+    (str "(" clausula-final ")"))) ; Envolve em parênteses
+  
+;; Função "despachante": decide o que fazer com base no tipo de entrada
+  (defn processa-clausula [clausula]
+  (cond
+    ;; Se for um mapa é uma condição simples
+    (map? clausula)
+    (processa-condicao clausula)
+
+    ;; Se for uma lista começando com 'e_s'
+    (and (list? clausula) (= 'e_s (first clausula)))
+    (processa-e-ou clausula " AND ")
+
+    ;; Se for uma lista começando com 'ou_s'
+    (and (list? clausula) (= 'ou_s (first clausula)))
+    (processa-e-ou clausula " OR ")
+
+    :else
+    (throw (Exception. (str "Cláusula inválida: " clausula)))))
+  
+ ;; --- Teste no REPL ---
+(def filtro-exemplo
+  (list 'e_s [{:campo :idade, :maior_que 20}
+              (list 'ou_s [{:campo :camiseta, :igual_a "verde"}
+                           {:campo :camiseta, :igual_a "azul"}])]))
+
+(processa-clausula filtro-exemplo)
